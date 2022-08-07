14cb9f0eb291772f940ff4450f3f3a87e345728a
marcellodomenis <admin@marcellodomenis.com>
2022-08-07 20:42:49 +0200

    flake template

--- ci.nix
+++ ci.nix
@@ -1,3 +1,23 @@
-if builtins?getFlake
-then (builtins.getFlake (toString ./.)).ciNix
-else (import ./flake-compat.nix).defaultNix.ciNix
+# if builtins?getFlake
+# then (builtins.getFlake (toString ./.)).ciNix
+# else (import ./flake-compat.nix).defaultNix.ciNix
+
+builtins.mapAttrs (k: _v:
+  let
+    url = "https://github.com/NixOS/nixpkgs/archive/2255f292063ccbe184ff8f9b35ce475c04d5ae69.tar.gz";
+    pkgs = import (builtins.fetchTarball url) { system = k; };
+  in
+    pkgs.recurseIntoAttrs {
+      # These two attributes will appear in your job for each platform.
+      hello = pkgs.hello;
+      cow-hello = pkgs.runCommand "cow-hello" {
+        buildInputs = [ pkgs.hello pkgs.cowsay ];
+      } ''
+      hello | cowsay > $out
+    '';
+    }
+) {
+  # x86_64-linux = {};
+  # Uncomment to test build on macOS too
+  aarch64-darwin = {};
+}
